7a547d20af0504cc66b3d7177f197840c78c3f59
Colton Shirley <coltonshirley31@gmail.com>
2023-09-06 20:12:27 -0700

    code makes console RPS

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
-console.log("hello world");
+//first function is get computer choice
+//write a prompt for user input
+//new function called game (nest the playRound function IN this one)
+//lastly, loop the function to play 5 rounds of the game
+
+let choice = ["rock", "paper", "scissors"] // this works
+
+console.log(choice)
+let randomNum = Math.floor(Math.random() * choice.length) // this works
+
+console.log(randomNum) 
+
+const playerSelection = prompt("rock paper scissors")
+
+function getPlayerSelection() {
+
+    if (userAnswer === "rock") {
+    return "rock";
+    }
+    else if (userAnswer === "paper") {
+    return "paper";
+}
+    else {
+        return "scissors";
+    }
+        
+    }
+
+console.log(playerSelection) // this works
+
+    function getComputerChoice() {//this function works!
+        let choice =  ["rock", "paper", "scissors"]
+        if (randomNum == 0)
+        return "rock";
+        else if (randomNum == 1)
+        return "paper";
+        else 
+        return "scissors";
+    }
+
+
+console.log(getComputerChoice())
+
+
+
+  const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {   // This works 
+     if (playerSelection === "rock" && computerSelection === "scissors" ||
+     playerSelection === "paper" && computerSelection === "rock" ||
+     playerSelection === "scissors" && computerSelection === "paper") {
+
+     return "good job you win this time";
+     }
+
+     else if (playerSelection === "scissors" && computerSelection === "rock" ||
+     playerSelection === "rock" && computerSelection === "paper" ||
+     playerSelection === "paper" && computerSelection === "scissors") {
+        
+        return "you lose this round";
+     }
+
+     else {
+        return "it's a tie";
+     }
+}
+
+console.log(playRound(playerSelection, computerSelection)) // all functions working
+
+
+
